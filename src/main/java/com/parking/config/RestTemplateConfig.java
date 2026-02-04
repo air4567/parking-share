016b654bb4286b635ceddb3efd620030c1439131
Airra <air2457638740@162.com>
2026-02-04 13:47:21 +0800

    修改证书验证

--- src/main/java/com/parking/config/RestTemplateConfig.java
+++ src/main/java/com/parking/config/RestTemplateConfig.java
@@ -2,7 +2,14 @@ package com.parking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,5 +17,40 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 仅用于调用微信 api.weixin.qq.com，解决云环境 JVM 信任库不完整导致的 SSLHandshakeException。
+     */
+    @Bean("wechatRestTemplate")
+    public RestTemplate wechatRestTemplate() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+        final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory() {
+            @Override
+            protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
+                super.prepareConnection(connection, httpMethod);
+                if (connection instanceof HttpsURLConnection) {
+                    ((HttpsURLConnection) connection).setSSLSocketFactory(sslSocketFactory);
+                }
+            }
+        };
+        return new RestTemplate(factory);
     }
 }
--- src/main/java/com/parking/service/WechatService.java
+++ src/main/java/com/parking/service/WechatService.java
@@ -3,25 +3,29 @@ package com.parking.service;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.parking.config.WechatConfig;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class WechatService {
 
     private final WechatConfig wechatConfig;
-    private final RestTemplate restTemplate;
+    private final RestTemplate wechatRestTemplate;
+
+    public WechatService(WechatConfig wechatConfig, @Qualifier("wechatRestTemplate") RestTemplate wechatRestTemplate) {
+        this.wechatConfig = wechatConfig;
+        this.wechatRestTemplate = wechatRestTemplate;
+    }
 
     private static final String CODE2SESSION_URL = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
 
     public String getOpenid(String code) {
         String url = String.format(CODE2SESSION_URL, wechatConfig.getAppid(), wechatConfig.getSecret(), code);
         try {
-            String resp = restTemplate.getForObject(url, String.class);
+            String resp = wechatRestTemplate.getForObject(url, String.class);
             JSONObject obj = JSON.parseObject(resp);
             if (obj.containsKey("errcode") && obj.getIntValue("errcode") != 0) {
                 log.warn("wechat code2session err: {}", resp);
